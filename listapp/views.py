54330d8b8ac5bf3d253267e1b87581cf7c37206e
simon-bauer <bauer.simon@gmx.de>
2023-01-02 22:08:21 +0100

    Enable checkbox

--- listapp/views.py
+++ listapp/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from .models import ShoppingItem
 from .models import ShoppingListItem
+import json
 
 def listapp(request):
     if request.method == 'POST':
         print('received data:', request.POST['itemName'])
         new_shopping_item = ShoppingItem.objects.create(name=request.POST['itemName'])
         ShoppingListItem.objects.create(shopping_item=new_shopping_item)
+    elif request.method == 'PUT':
+        request_body = json.loads(request.body.decode('utf-8'))
+        print(request_body['itemName'])
+        item = ShoppingListItem.objects.get(shopping_item__name=request_body['itemName'])
+        item.done = not item.done
+        item.save()
     all_items = ShoppingListItem.objects.all()
     return render(request, 'list.html', {'all_items': all_items})
     
