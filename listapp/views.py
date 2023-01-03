41cdd128a1e45c3d57ab2c4dcca149a1b29a60ff
simon-bauer <bauer.simon@gmx.de>
2023-01-03 19:22:52 +0100

    add delete button

--- listapp/views.py
+++ listapp/views.py
@@ -11,9 +11,13 @@ def listapp(request):
     elif request.method == 'PUT':
         request_body = json.loads(request.body.decode('utf-8'))
         print(request_body['itemName'])
-        item = ShoppingListItem.objects.get(shopping_item__name=request_body['itemName'])
-        item.done = not item.done
-        item.save()
+        for item in ShoppingListItem.objects.filter(shopping_item__name=request_body['itemName']):
+            item.done = not item.done
+            item.save()
+    elif request.method == 'DELETE':
+        for item in ShoppingListItem.objects.filter(done=True):
+            item.delete()
+        
     all_items = ShoppingListItem.objects.all()
     return render(request, 'list.html', {'all_items': all_items})
     
